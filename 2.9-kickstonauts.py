9871c111a3cb1028feba38142b97daa0db61c32c
Jon Carr <jonnydc4@gmail.com>
2020-09-27 15:36:25 -0600

    Added two assignments previously completed

---
+++ 2.9-kickstonauts.py
@@ -0,0 +1,5 @@
+print("hello")
+kilo = int(input("Enter the number of kilometers:"))
+roc = float((90 * 60) / 10000)
+nau = kilo * roc
+print("The number of nautical miles is", nau)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CS1400
+
+This repository contains all the programming assignments I compled for UVU class CS1400.
